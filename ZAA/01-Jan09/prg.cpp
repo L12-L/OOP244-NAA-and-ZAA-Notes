a036b0d9a85541f09521db8379d9e62ca12dabb9
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-01-09 13:54:22 -0500

    added "and github"

--- ZAA/01-Jan09/prg.cpp
+++ ZAA/01-Jan09/prg.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-   cout << "Welcome to oop244 zaa" << endl;
+   cout << "Welcome to oop244 zaa" << endl
+      << "and github!" << endl;
    return 0;
 }
