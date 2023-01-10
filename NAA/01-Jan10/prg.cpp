9f08e5951b63842917b6211f28dd3b5a71ee526f
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-01-10 10:59:05 -0500

    welcome

--- NAA/01-Jan10/prg.cpp
+++ NAA/01-Jan10/prg.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-   cout << "Hello oop244naa" << endl;
+   cout << "Hello oop244naa" << endl
+      << "Welcome to 2231 semester!" << endl;
    return 0;
 }
